7500bdc5d73ec166bf8661bc881cb9bbd1b7aef3
AlexRamlogan02 <AlexRamlogan02@knights.ucf.edu>
2021-10-11 19:32:35 -0400

    hey

--- src/main/java/ex42/App.java
+++ src/main/java/ex42/App.java
@@ -6,15 +6,21 @@ package ex42;
  */
 
 import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class App {
     public static void main( String[] args ) {
         List<String> input;
+        List<String> finals;
         FileWriter output;
-
         try{
-
+            input = Files.readAllLines(Paths.get("src/main/java/ex42/exercise42_input.txt"), StandardCharsets.US_ASCII);
+            for (int i = 0; i < input.size(); i++) {
+                System.out.print(input.get(i) + "\n"); //test code
+            }
         }
         catch (Exception e){System.out.print("fail\n");}
     }
