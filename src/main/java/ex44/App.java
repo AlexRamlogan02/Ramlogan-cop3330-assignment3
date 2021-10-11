5403a290e8c5d1ca3ee82525d36d6365fe2531ae
AlexRamlogan02 <AlexRamlogan02@knights.ucf.edu>
2021-10-11 19:48:21 -0400

    hey

--- src/main/java/ex44/App.java
+++ src/main/java/ex44/App.java
@@ -1,8 +1,20 @@
 package ex44;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main( String[] args ) {
-
+        //read in data
+        String findProduct;
+        Scanner scan = new Scanner(System.in);
+        int found = 0;
+        while (found != 1)
+        {
+            System.out.print("What is the product name? ");
+            findProduct = scan.nextLine();
+        };
+        //if the product isnt found, continue the while  loop
+        //else (found), exit the while loop & print the name, price & quantity
     }
 }
